f054a75046d3a0c79425d089274754e414ad2f57
John Majka Jr. <jmjr97@gmail.com>
2024-10-02 11:27:50 -0400

    working on mobspawner

--- mobspawner/main.js
+++ mobspawner/main.js
@@ -1,8 +1,31 @@
-async function getCR() {
+const formEntry = document.querySelector('form');
+const combatRating = document.getElementsByClassName('inputSection');
+const nameInput = document.getElementById('nameInput');
+
+async function getMobList() {
   const response = await fetch('https://jmjr97.github.io/mobspawner/cr.json');
-  const cr =  await response.json();
-  console.log(cr);
+  json = await response.json();
+  return json;
 }
 
-getCR();
+const crList = getMobList();
 
+formEntry.addEventListener('submit', function(e) {
+  e.preventDefault();
+  addMob(combatRating);
+})
+
+// function addMob(combatRating) {
+//   const nameText = nameInput.value.trim();
+//   if(nameText) {
+//     const mobObject = {
+//       name: nameText,
+//       armorClass: crList.[combatRating].AC,
+//     }
+//     updateMobs();
+//   }
+// }
+
+
+
+console.log(crList.one.AC)
